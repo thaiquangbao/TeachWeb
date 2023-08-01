c5f733f67d0ffa58e69892cfb682a947ca0d66b5
thaiquangbao <97949012+thaiquangbao@users.noreply.github.com>
2023-08-01 11:28:16 +0700

    upload

--- node/src/config/db/index.js
+++ node/src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/20093191');
+    await mongoose.connect(process.env.MONGO_DB);
     console.log('Connect thành công !!!');
   } catch (error) {
     console.log('Connect không thành công');
